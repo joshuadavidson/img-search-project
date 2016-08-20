3925b40018fe64d1ddd9dace9237f6dcc933296e
Joshua Davidson <joshuadavidson@users.noreply.github.com>
2016-08-19 20:44:46 -0500

    Completed project.

--- routes/history.js
+++ routes/history.js
@@ -4,9 +4,32 @@ const router = express.Router();
 
 const mlabURI = process.env.MLAB_URI; //get the DB url from envrionment variable
 
-router.get('/', function(req, res){
+router.get('/', function(req, res, next) {
 
-  res.send(mlabURI);
+  var limit = parseInt(req.query.limit) || 10; //get the limit from the query or default to 10
+
+  mongo.connect(mlabURI).then(function(db) {
+    var imgSearchHistory = db.collection('imgSearchHistory'); //get the image search history colleciton
+
+    imgSearchHistory.find({}, {
+        _id: false
+      })
+      .limit(limit) //limit results
+      .sort({ $natural: -1 }) //sort so that most recent shows at top of JSON
+      .toArray() //convert results to an array
+
+    //once docs are received display them
+    .then(docs => {
+      res.json(docs); //return the json response of history docs
+      db.close(); //close the db
+    })
+
+    //catch any errors
+    .catch(err => {
+      return next(err);
+    });
+  });
+
 });
 
 module.exports = router;
